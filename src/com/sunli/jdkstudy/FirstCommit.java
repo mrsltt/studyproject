d063be405f4d33e3df0949fcaa15a38dfd5a0c94
mrsl <361273520@qq.com>
2019-04-15 15:02:29 +0800

    firstcommit this

--- src/com/sunli/jdkstudy/FirstCommit.java
+++ src/com/sunli/jdkstudy/FirstCommit.java
@@ -1,10 +1,21 @@
 package com.sunli.jdkstudy;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class FirstCommit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		InputStream io = new InputStream() {
+			
+			@Override
+			public int read() throws IOException {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		};
 	}
 
 }
